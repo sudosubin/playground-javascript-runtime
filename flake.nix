9d46880df66977f8386dcd663ba8061e160c7840
sudosubin <sudosubin@gmail.com>
2025-03-30 20:12:01 +0900

    feat: update nix flake

--- flake.nix
+++ flake.nix
@@ -2,22 +2,29 @@
   description = "sudosubin/playground-javascript-runtime";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, rust-overlay }:
     let
       inherit (nixpkgs.lib) genAttrs platforms;
-      forAllSystems = f: genAttrs platforms.unix (system: f (import nixpkgs { inherit system; }));
+      forAllSystems = f: genAttrs platforms.unix (system: f (import nixpkgs {
+        inherit system;
+        overlays = [ (import rust-overlay) ];
+      }));
 
     in
     {
       devShells = forAllSystems (pkgs: {
         default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            cargo
-            libiconv
-            rustfmt
+            (rust-bin.stable."1.62.1".default.override {
+              extensions = [ "rust-src" ];
+            })
           ];
         };
       });
